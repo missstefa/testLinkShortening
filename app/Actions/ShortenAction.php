e9cc848271d975a5f2f931c771cb5e01810d73ac
missstefa <miss.stefa1517@gmail.com>
2022-01-25 21:42:57 +0300

    fixes

--- app/Actions/ShortenAction.php
+++ app/Actions/ShortenAction.php
@@ -2,27 +2,22 @@
 
 namespace App\Actions;
 
-class ShorteningAction
+class ShortenAction
 {
     public const LINK_SHORTENING_SITES = ["http://www.vybor.by/"];
 
     public static function execute(string $url)
     {
         foreach (self::LINK_SHORTENING_SITES as $key => $linkShorteningSite) {
-
             if ($key == array_key_last(self::LINK_SHORTENING_SITES) && !CheckSiteAction::checkLive($linkShorteningSite)) {
-                return back()->withError('Site is unavailable');
+                throw new \Exception('Sites is unavailable');
             }
 
             if (!CheckSiteAction::checkLive($linkShorteningSite)) {
                 continue;
             }
 
-            try {
-                return file_get_contents($linkShorteningSite.'api-create.php?url='.$url);
-            } catch (\Exception $exception) {
-                return back()->withError($exception->getMessage());
-            }
+            return file_get_contents($linkShorteningSite.'api-create.php?url='.$url);
         }
     }
 }
--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Actions\ShorteningAction;
+use App\Actions\ShortenAction;
 use App\Http\Requests\Link\ShortenRequest;
 use Illuminate\Routing\Controller;
 
 class LinkController extends Controller
 {
-    public function shorten(ShortenRequest $request) {
-        return view('welcome', ['shortLink' => ShorteningAction::execute($request->getLink())]);
+
+    public function shorten(ShortenRequest $request)
+    {
+        try {
+            return view('welcome', ['shortLink' => ShortenAction::execute($request->getLink())]);
+        } catch (\Exception $exception) {
+            return back()->withError($exception->getMessage());
+        }
     }
 }
