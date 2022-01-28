3121cb7d8691e1c7481c4b49f6dac92608077ce8
missstefa <miss.stefa1517@gmail.com>
2022-01-28 14:09:24 +0300

    fixes

--- app/Services/LinkService.php
+++ app/Services/LinkService.php
@@ -8,7 +8,6 @@ class LinkService
 {
     public function linkPostfixGenerate (): string
     {
-        return('4r5tYU');
         return str_shuffle(Str::upper(Str::random(2)).Str::lower(Str::random(2)).mt_rand(10, 99));
     }
 }
